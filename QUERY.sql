379d33b304a0de4da7e44a386578530658d3f2d5
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2026-06-14 21:54:27 +0600

    query 3-7

--- QUERY.sql
+++ QUERY.sql
@@ -70,3 +70,46 @@ SELECT booking_id,user_id,match_id,
 COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM Bookings
 WHERE payment_status IS NULL;
+
+- QUERY 4 
+
+
+SELECT b.booking_id,u.full_name,m.fixture,b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id
+ORDER BY b.booking_id;
+
+
+
+--  QUERY 5 
+
+SELECT u.user_id,u.full_name,b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id, b.booking_id;
+
+
+
+-- QUERY 6 
+
+
+SELECT booking_id,match_id,total_cost
+FROM Bookings
+WHERE total_cost > (
+    SELECT AVG(total_cost) 
+    FROM Bookings
+)
+ORDER BY total_cost DESC;
+
+
+--  QUERY 7 
+
+
+SELECT match_id,fixture,base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1;
+
+
+
